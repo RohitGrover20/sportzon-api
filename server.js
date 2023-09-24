4faf991f2922d56e8789f8b8c0c457a4847ed371
rahulyesteq <rahul@yesteq.co.in>
2023-09-24 23:24:54 +0530

    None to none

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.use(
     name: "sessionId",
     keys: ["thisismysecrctekeyfhrgfgrfrty84fwir767"],
     maxAge: 24 * 60 * 60 * 100,
-    sameSite: "None",
+    sameSite: "none",
     secure: true,
   })
 );
