3d2b8348cfb1ad126092f0f7780521f938dd028e
rahulyesteq <rahul@yesteq.co.in>
2023-09-25 22:08:50 +0530

    newone

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.use(
     name: "sessionId",
     keys: ["thisismysecrctekeyfhrgfgrfrty84fwir767"],
     maxAge: 24 * 60 * 60 * 100,
-    domain: "chicagomegashop.com",
+    domain: "sportzon.in",
     sameSite: "none",
     secure: true,
     httpOnly: true,
