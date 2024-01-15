9e8dc39b15d2c7fadfae824a891f4838c6cc23ea
Yesteq-Ventures <rahul@yesteq.co.in>
2024-01-15 13:29:02 +0530

    changes to dashboard

--- dashboard/Controller.js
+++ dashboard/Controller.js
@@ -1,18 +1,29 @@
 const Events = require("../events/Model");
 const Users = require("../users/Model");
 const Coaches = require("../coaches/Model");
-const {Booking} = require("../bookings/Model");
+const { Booking } = require("../bookings/Model");
 const Students = require("../students/Model");
 const Clubs = require("../club/Model");
 
 const MONTHS = [
-  "January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
 const getMonthIndex = (month) => MONTHS.indexOf(month);
 module.exports = {
   getDashboard: async (req, res) => {
     try {
+      const currentYear = new Date().getFullYear();
       // Fetch data from various models concurrently
       const [
         totalEvents,
@@ -22,7 +33,7 @@ module.exports = {
         totalStudents,
         totalAcademies,
         monthlyEventCounts,
-        monthlyUserCounts
+        monthlyUserCounts,
       ] = await Promise.all([
         Events.countDocuments(),
         Users.countDocuments(),
@@ -31,19 +42,78 @@ module.exports = {
         Students.countDocuments(),
         Clubs.countDocuments(),
         Events.aggregate([
-          { $match: { eventDate: { $exists: true, $ne: null } } },
-          { $group: { _id: { $month: { $toDate: "$eventDate" } }, count: { $sum: 1 } } },
-          { $project: { month: { $let: { vars: { monthArray: MONTHS }, in: { $arrayElemAt: ["$$monthArray", { $subtract: ["$_id", 1] }] } } }, count: 1, _id: 0 } },
+          {
+            $match: {
+              eventDate: {
+                $exists: true,
+                $ne: null,
+                $gte: new Date(`${currentYear}-01-01`),
+                $lt: new Date(`${currentYear + 1}-01-01`),
+              },
+            },
+          },
+          {
+            $group: {
+              _id: { $month: { $toDate: "$eventDate" } },
+              count: { $sum: 1 },
+            },
+          },
+          {
+            $project: {
+              month: {
+                $let: {
+                  vars: { monthArray: MONTHS },
+                  in: {
+                    $arrayElemAt: ["$$monthArray", { $subtract: ["$_id", 1] }],
+                  },
+                },
+              },
+              count: 1,
+              _id: 0,
+            },
+          },
         ]),
         Users.aggregate([
-          { $group: { _id: { $month: { $toDate: "$createdAt" } }, count: { $sum: 1 } } },
-          { $project: { month: { $let: { vars: { monthArray: MONTHS }, in: { $arrayElemAt: ["$$monthArray", { $subtract: ["$_id", 1] }] } } }, count: 1, _id: 0 } },
+          {
+            $match: {
+              createdAt: {
+                $gte: new Date(`${currentYear}-01-01`),
+                $lt: new Date(`${currentYear + 1}-01-01`),
+              },
+            },
+          },
+          {
+            $group: {
+              _id: { $month: { $toDate: "$createdAt" } },
+              count: { $sum: 1 },
+            },
+          },
+          {
+            $project: {
+              month: {
+                $let: {
+                  vars: { monthArray: MONTHS },
+                  in: {
+                    $arrayElemAt: ["$$monthArray", { $subtract: ["$_id", 1] }],
+                  },
+                },
+              },
+              count: 1,
+              _id: 0,
+            },
+          },
         ]),
       ]);
 
       // Initialize arrays with counts set to 0 for all months
-      const monthlyEventCountsArray = Array.from({ length: 12 }, (_, monthIndex) => ({ month: MONTHS[monthIndex], count: 0 }));
-      const monthlyUserCountsArray = Array.from({ length: 12 }, (_, monthIndex) => ({ month: MONTHS[monthIndex], count: 0 }));
+      const monthlyEventCountsArray = Array.from(
+        { length: 12 },
+        (_, monthIndex) => ({ month: MONTHS[monthIndex], count: 0 })
+      );
+      const monthlyUserCountsArray = Array.from(
+        { length: 12 },
+        (_, monthIndex) => ({ month: MONTHS[monthIndex], count: 0 })
+      );
 
       // Update counts based on actual data
       monthlyEventCounts.forEach((result) => {
@@ -85,4 +155,3 @@ module.exports = {
     }
   },
 };
-
