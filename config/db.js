b42c5f98b618df028ef48cb4a9bc2f4238f033c3
rahulyesteq <141490282+rahulyesteq@users.noreply.github.com>
2024-02-21 15:14:16 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,28 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-// mongoose.set('useCreateIndex', true);
 
 const password = encodeURIComponent(process.env.DBPASSWORD);
+
 module.exports = function () {
   mongoose.connect(
     `mongodb+srv://${process.env.DBUSER}:${password}@yesteq.lt5yiuc.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`,
-    { useNewUrlParser: true, useUnifiedTopology: true, ignoreUndefined: true }
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useCreateIndex: true // Uncomment if needed
+    }
   );
+
   mongoose.connection.on("connected", () => {
-    console.log("db connected");
+    console.log("MongoDB connected");
+  });
+
+  mongoose.connection.on("error", (err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
+  mongoose.connection.on("disconnected", () => {
+    console.log("MongoDB disconnected");
   });
 };
+
